d198c85f4b05c04bf9e7ddb7ddb14b6ef50df78d
nadotdev <nadotdev@example.com>
2024-02-08 11:23:27 +0700

    modified ceramic form v2 & urls

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "home"
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),# url homepage
-    path("ceramic-app", views.ceramic_app, name="Ceramic application"), # ceramic app
+    path("inventory_of_pottery_maker", views.ceramic_app, name="Ceramic application"), # ceramic app
     path("kampot", views.kampot_view, name="Ceramic kampot page"), # kampot page
 ]
