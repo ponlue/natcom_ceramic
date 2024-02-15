39e944ff0cc62aef0cf2f9177d94072020285171
nadotdev <nadotdev@example.com>
2024-02-15 11:14:18 +0700

    rename docker and container in run-web.sh

--- run-web.sh
+++ run-web.sh
@@ -9,6 +9,6 @@
 
 sudo docker container stop sothatna-ceramic-container
 sudo docker container rm sothatna-ceramic-container
-sudo docker image rm sothatna-ceramic-ceramic:2024
-sudo docker build . -t="sothatna-img-app:2024"
-sudo docker run -it -d -p 5063:8000 --name sothatna-ceramic-container --mount type=bind,source="$(pwd)",target=/web sothatna-img-app:2024
+sudo docker image rm sothatna-ceramic-img:2024
+sudo docker build . -t sothatna-ceramic-img:2024
+sudo docker run -it -d -p 5063:8000 --name sothatna-ceramic-container --mount type=bind,source="$(pwd)",target=/web sothatna-ceramic-img:2024
