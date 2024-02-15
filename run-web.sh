1eaf957801a5dfedceacf08dd4ff32737fcb02c8
nadotdev <nadotdev@example.com>
2024-02-15 11:19:21 +0700

    fix run web command

--- run-web.sh
+++ run-web.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-# sudo rm -rf web
-# sudo mkdir web
-# sudo cp -r ceramic web
-# sudo cp -r home web
-# sudo cp manage.py web
-# sudo cp -r static web
-# sudo chown natcom:natcom_sothatna -R web
-
 sudo docker container stop sothatna-ceramic-container
 sudo docker container rm sothatna-ceramic-container
 sudo docker image rm sothatna-ceramic-img:2024
