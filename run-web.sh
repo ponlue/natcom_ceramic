de986f0d230379bb79dcc6efc788e2a6e33bc5d3
nadotdev <nadotdev@example.com>
2024-02-15 10:59:07 +0700

    update run-web cmd

--- run-web.sh
+++ run-web.sh
@@ -7,8 +7,8 @@
 # sudo cp -r static web
 # sudo chown natcom:natcom_sothatna -R web
 
-sudo docker container stop sothatna-webapp-container
-sudo docker container rm sothatna-webapp-container
-sudo docker image rm sothatna-img-app:2024
+sudo docker container stop sothatna-ceramic-container
+sudo docker container rm sothatna-ceramic-container
+sudo docker image rm sothatna-ceramic-ceramic:2024
 sudo docker build . -t="sothatna-img-app:2024"
-sudo docker run -it -d -p 5063:8000 --name sothatna-webapp-container --mount type=bind,source="$(pwd)",target=/web sothatna-img-app:2024
+sudo docker run -it -d -p 5063:8000 --name sothatna-ceramic-container --mount type=bind,source="$(pwd)",target=/web sothatna-img-app:2024
