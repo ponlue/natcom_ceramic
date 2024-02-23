1f8bf7c65e2e98c87b62ea268a9a805d9caf38e5
nadotdev <nadotdev@example.com>
2024-02-23 11:50:02 +0700

    change mysql configure

--- ceramic/settings.py
+++ ceramic/settings.py
@@ -77,19 +77,19 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
 
-        # 'ENGINE': 'mysql.connector.django',
-        # 'NAME': 'db_ceramic_testing',
-        # 'USER': 'db_user_ceramic',
-        # 'PASSWORD': 'C#293()239#@AbB',
-        # 'HOST': 'database.krissna.com',
-        # 'PORT': '211',
-
         'ENGINE': 'mysql.connector.django',
-        'NAME': 'ceramic_db',
-        'USER': 'nadotdev',
-        'PASSWORD': 'Tana@321',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'NAME': 'db_ceramic_testing',
+        'USER': 'db_user_ceramic',
+        'PASSWORD': 'C#293()239#@AbB',
+        'HOST': 'database.krissna.com',
+        'PORT': '211',
+
+        # 'ENGINE': 'mysql.connector.django',
+        # 'NAME': 'ceramic_db',
+        # 'USER': 'nadotdev',
+        # 'PASSWORD': 'Tana@321',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '3306',
     }
 }
 
