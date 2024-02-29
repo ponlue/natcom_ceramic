ec0dd80d10b27be976989caef85e6043c949a087
Reach Ponlue <reachponlue.com>
2024-02-29 09:16:01 +0000

    fix error catche

--- README.md
+++
@@ -1,1 +0,0 @@
-# natcom_ceramic
--- run-web.sh
+++
@@ -1,5 +0,0 @@
-sudo docker container stop ceramic-container-testing
-sudo docker container rm ceramic-container-testing
-sudo docker image rm ceramic-img-testing:2024
-sudo docker build -t ceramic-img-testing:2024 .
-sudo docker run -it -d -p 9999:9999 --name ceramic-container-testing --mount type=bind,source="$(pwd)",target=/web ceramic-img-testing:2024
