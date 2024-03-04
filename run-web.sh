19aff0ba07140e456ff5cc35fa9c68964311e9b1
nadotdev <nadotdev@example.com>
2024-03-04 11:04:21 +0700

    Change docker port

--- run-web.sh
+++ run-web.sh
@@ -2,4 +2,4 @@ sudo docker container stop ceramic-container-testing-v2
 sudo docker container rm ceramic-container-testing-v2
 sudo docker image rm ceramic-img-v2:2024
 sudo docker build . -t ceramic-img-v2:2024
-sudo docker run -it -d -p 2304:8000 --name ceramic-container-testing-v2 --mount type=bind,source="$(pwd)",target=/ceramic ceramic-img-v2:2024
+sudo docker run -it -d -p 5063:8000 --name ceramic-container-testing-v2 --mount type=bind,source="$(pwd)",target=/ceramic ceramic-img-v2:2024
