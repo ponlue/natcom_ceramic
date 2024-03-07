b1fed4526fff6bfe32b76f9d973b611ceec37489
nadotdev <nadotdev@example.com>
2024-03-07 10:23:29 +0700

    add list per page in admin site

--- home_slideshow/admin.py
+++ home_slideshow/admin.py
@@ -4,5 +4,6 @@ from .models import HomeSlideShow
 
 class SlideshowAdmin(admin.ModelAdmin):
     list_display = ('id', 'image_tag')
+    list_per_page = 5
 
 admin.site.register(HomeSlideShow, SlideshowAdmin)
