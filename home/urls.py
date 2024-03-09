de8d4a7cb452cdb782e8997325ea179c1182cfe0
nadotdev <nadotdev@example.com>
2024-03-09 12:14:11 +0700

    fix model and admin site

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("potter", views.potter, name="Potter application"),  # ceramic app
     path("howto", views.technique_making_potter_list, name="technique making potter list"),  # test technique_making_potter
     path('success-submitted', views.success_submitted_potter_form),
+    path('img-collection', views.image_collection),
+
+
 ]
